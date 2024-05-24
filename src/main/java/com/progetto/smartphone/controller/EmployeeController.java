9520904d0c499d39a30dc04c538e7e93e2ad9305
Hiroit34 <franz.natavio@gmail.com>
2024-05-24 16:57:58 +0200

    Finito

--- src/main/java/com/progetto/smartphone/controller/EmployeeController.java
+++ src/main/java/com/progetto/smartphone/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.progetto.smartphone.controller;
 import com.progetto.smartphone.dto.EmployeeDTO;
 import com.progetto.smartphone.entity.Employee;
 import com.progetto.smartphone.errorHandling.ResourceNotFoundException;
+import com.progetto.smartphone.repository.EmployeeRepository;
 import com.progetto.smartphone.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +26,10 @@ public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
+
+    private static String UPLOAD_DIR = "profile-image";
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @GetMapping
     public List<Employee> getAllEmployees() {
@@ -57,4 +68,46 @@ public class EmployeeController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PostMapping("/{id}/uploadProfileImage")
+    public ResponseEntity<String> uploadProfileImage(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return new ResponseEntity<>("Please select a file to upload", HttpStatus.BAD_REQUEST);
+        }
+        try {
+            Path uploadPath = Paths.get(UPLOAD_DIR);
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+                System.out.println("Created directory: " + uploadPath.toString());
+            } else {
+                System.out.println("Directory already exists: " + uploadPath.toString());
+            }
+
+            String fileName = id + "_" + file.getOriginalFilename();
+            Path filePath = uploadPath.resolve(fileName);
+            System.out.println("Resolved file path: " + filePath.toString());
+
+            Files.write(filePath, file.getBytes());
+            System.out.println("File written: " + filePath.toString());
+
+            Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+            if (!optionalEmployee.isPresent()) {
+                return new ResponseEntity<>("Employee not found", HttpStatus.NOT_FOUND);
+            }
+
+            Employee employee = optionalEmployee.get();
+            employee.setProfileImagePath(filePath.toString());
+            employeeRepository.save(employee);
+
+            return new ResponseEntity<>("Successfully uploaded - " + file.getOriginalFilename(), HttpStatus.OK);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Failed to upload file", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("An error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
+
